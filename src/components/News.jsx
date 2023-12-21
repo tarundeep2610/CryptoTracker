c8465596bf4d241e984fa69475dcf64ec571e616
Tarundeep <manavtarundeep2001@gmail.com>
2023-12-21 18:53:55 +0530

    updated previous news api with another api because previous one has cors policy that don't allowed to fetch via netlify

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -11,11 +11,11 @@ const { Option } = Select;
 const demoImage= 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
 
 export default function News({simplified}){
-    const[newsCategory, setNewsCategory]= useState('Cryptocurrency')
+    const[newsCategory, setNewsCategory]= useState('')
 
-     const {data} = useGetCryptosQuery(100);
+    const {data} = useGetCryptosQuery(100);
 
-    const {data: cryptoNews, isFetching} = useGetCryptoNewsQuery({newsCategory:newsCategory, count: simplified?6:12});
+    const {data: cryptoNews, isFetching} = useGetCryptoNewsQuery({newsCategory:newsCategory, count: simplified?6:20});
 
     if(isFetching){
         return <Loader></Loader>;
@@ -27,24 +27,28 @@ export default function News({simplified}){
                 <Col span={24}>
                     <Select showSearch className='select-news' placeholder='Select a Crypto' optionFilterProp='children' onChange={(value) => setNewsCategory(value)} filterOption= {(input,option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}>
                         <Option value='Cryptocurrency'>Cryptocurrency</Option>
-                        {data?.data?.coins.map((coin,index) => <Option value={coin.name} key={index}>{coin.name}</Option>)}
+                        {data?.data?.coins.map((coin,index) => <Option value={coin.symbol} key={index}>{coin.name}</Option>)}
                     </Select>
                 </Col>
             )
             }
-            { cryptoNews?.articles?.map((news,i) => (
+            {
+            cryptoNews?.news?.map((data,i) => (
                 <Col xs={24} sm={12} lg={8} key={i}>
                     <Card hoverable className='news-card'>
-                        <a href={news.url} target='_blank' rel='noreferrer'>
+                        <a href={data.Url} target='_blank' rel='noreferrer'>
                             <div className="news-image-container">
-                                <Title className='news-title' level={4}>{news.title}</Title>
-                                <img src={news?.urlToImage || demoImage} alt='news' style={{maxWidth:'150px', maxHeight:'100px'}}></img>
+                                <Title className='news-title' level={4}>{data.Title}</Title>
+                                <img src={data?.Image || demoImage} alt='news' style={{maxWidth:'150px', maxHeight:'100px'}}></img>
                             </div>
                             <p>
-                                {news.description > 100 ? `${news.description.substring(0,100)}...` : news.description};
+                                {data.description > 100 ? `${data.description.substring(0,100)}...` : data.Description};
                             </p>
                             <div className="provider-container">
-                                <Text>{moment(news.publishedAt).startOf('ss').fromNow()}</Text>
+                                
+                                <Text className="provider-name">{data?.Source}</Text>
+                                
+                                <Text>{moment(data.PublishedOn).startOf('ss').fromNow()}</Text>
                             </div>
                         </a>
                     </Card>
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,22 +1,30 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoNewsApiHeaders = {
-    'X-Api-Key': 'd08233df855c4f668ff7dc37599e3e60'
-}
+  "X-RapidAPI-Key": "3d1661a5fcmsha9d8b948c2961c3p1da69ejsn6422dd3de303",
+  "X-RapidAPI-Host": "news67.p.rapidapi.com",
+};
 
-const baseUrl= 'https://newsapi.org/v2';
+const baseUrl = "https://news67.p.rapidapi.com/v2";
 
-const createRequest = (url) => ({url, headers: cryptoNewsApiHeaders});
+const createRequest = (url) => ({ url, headers: cryptoNewsApiHeaders });
 
-export const cryptoNewsApi = createApi ({
-    reducerPath: 'cryptoNews',
-    baseQuery: fetchBaseQuery({baseUrl}),
-    endpoints: (builder) => ({
-        getCryptoNews: builder.query({
-            query: ({newsCategory,count}) =>  createRequest(`/everything?q=${newsCategory}&pageSize=${count}`)})
-    })
-})
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNews",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => {
+        if (newsCategory === "") {
+          return createRequest(`/crypto?languages=en&batchSize=${count}`);
+        }
 
-export const{
-    useGetCryptoNewsQuery
-} = cryptoNewsApi; 
+        return createRequest(
+          `/crypto?token=${newsCategory}&languages=en&batchSize=${count}`
+        );
+      },
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
